5ddb83f59990a1d332711c2bb72b25764057ffd4
LuizGlomyer <39037919+LuizGlomyer@users.noreply.github.com>
2018-05-09 13:06:21 -0300

    Add files via upload

--- Python/gerador_nomes.py
+++ Python/gerador_nomes.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import *
 n = int(input("insira a quantidade de nomes a serem gerados\n"))
 s = open("silabas.txt", "r")
 silabas = s.read()
@@ -7,13 +7,16 @@ nomes = []
 
 for nome in range(n):
     a = ""
-    tamanhonome = randint (2,5)
+    tamanhonome = randint(2,5)
     for silaba in range(tamanhonome):
-        a = a + silabas[randint(0, len(silabas) - 1)]
+        sil = choice(silabas)
+        while silaba == 0 and sil in "lrsxmn" or silaba == 0 and sil in ["rra", "rre", "rri", "rro", "rru", "ssa","sse", "ssi", "sso", "ssu"]: #a primeira silaba não pode começar com uma dessas consoantes
+            sil = choice(silabas)
+        a = a + sil
+        a = a.capitalize()
+
     nomes.append(a)
 
 print(nomes)
 
 s.close()
-
-input()
