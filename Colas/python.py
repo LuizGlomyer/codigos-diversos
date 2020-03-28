f5b605875364f8ebb56f433a3cb9950870390187
glomyer <glomyerjunior@hotmail.com>
2020-03-28 13:40:39 -0300

    alterações

--- Colas/python.py
+++
@@ -1,3 +0,0 @@
-"{0:.2f}".format(3,1415)  - duas casas decimais 
-"{:03d}" - inteiro com no mínimo 3 casas. 1 se torna 001
-dicionario = sorted(dicionario.items(), key=lambda x: x[1]) # ordenar um dict pelo valor
